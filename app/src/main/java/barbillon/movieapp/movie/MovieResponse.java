f7f407223f9f00cea8204e28e90f8904a6c266ed
Alex <alexandre.barbillon@gmail.com>
2019-11-06 09:37:33 +0100

    Creating API Manipulators
    This is a set of files used for requesting the API

---
+++ app/src/main/java/barbillon/movieapp/movie/MovieResponse.java
@@ -0,0 +1,39 @@
+package barbillon.movieapp.movie;
+
+import androidx.annotation.NonNull;
+
+import java.util.List;
+
+public class MovieResponse {
+
+    int pages;
+
+    List<MovieViewModel> results;
+
+    public MovieResponse(int pages, List<MovieViewModel> results) {
+        this.pages = pages;
+        this.results = results;
+    }
+
+    public int getPages() {
+        return pages;
+    }
+
+    public void setPages(int pages) {
+        this.pages = pages;
+    }
+
+    public List<MovieViewModel> getResults() {
+        return results;
+    }
+
+    public void setResults(List<MovieViewModel> results) {
+        this.results = results;
+    }
+
+    @NonNull
+    @Override
+    public String toString() {
+        return String.valueOf(this.results.size());
+    }
+}
