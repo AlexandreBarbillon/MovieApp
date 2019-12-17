d54c8fdd6a5476ff40233c203b23774f310d1e09
Alex <alexandre.barbillon@gmail.com>
2019-12-17 18:35:33 +0100

    Documentation + Resolving a crash
    When the phone is offline, the application crashed when the toggle view button was pushed

--- app/src/main/java/barbillon/movieapp/views/MainList.java
+++ app/src/main/java/barbillon/movieapp/views/MainList.java
@@ -23,13 +23,16 @@ import barbillon.movieapp.moviedisplay.ViewContract;
 import barbillon.movieapp.views.movieadapters.MovieGridItemAdapter;
 import barbillon.movieapp.views.movieadapters.MovieListItemAdapter;
 
+/**
+ * Affichage principal de l'application. Il contient une RecyclerView qui se met à jour suivant l'affichage souhaité, soit par grille, soit par list (contenant alors le titre et la date de sortie du film)
+ */
 public class MainList extends AppCompatActivity implements ViewContract {
 
     private RecyclerView movieList;
     private RecyclerView.LayoutManager layoutManager;
     private RecyclerView.Adapter movieListAdapter;
     private MoviePresenter moviePresenter;
-    private boolean isListView = true;
+    private boolean isListView = true; /** défini si l'affichage est une list ou une grille*/
     private List<MovieViewModel> movieViewModelList;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -49,23 +52,35 @@ public class MainList extends AppCompatActivity implements ViewContract {
         //movieListAdapter = new MovieListAdapter();
     }
 
+    /**
+     * Cette fonction correspond au contrat qu'implémente cette vue, c'est cette fonction qui est appellée lorsque l'appel à l'API est terminé dans le MoviePresenter.
+     * @param movieViewModelList
+     */
     @Override
     public void displayMovies(List<MovieViewModel> movieViewModelList) {
         this.movieViewModelList = movieViewModelList;
         displayMovies();
     }
 
+    /**
+     * Cette fonction actualise la RecyclerView avec les données présentent dans l'attribut movieViewModelList. Elle affichera alors une grille ou une liste suivant l'état de l'attribut isListView
+     */
     public void displayMovies(){
-        if(isListView){
-            movieList.setLayoutManager(this.layoutManager);
-            movieList.setAdapter(new MovieListItemAdapter(this,this.movieViewModelList,movieList));
-        }
-        else{
-            movieList.setLayoutManager((new GridLayoutManager(this, 2)));
-            movieList.setAdapter(new MovieGridItemAdapter(this, this.movieViewModelList,movieList));
+        if(movieViewModelList != null){
+            if(isListView){
+                movieList.setLayoutManager(this.layoutManager);
+                movieList.setAdapter(new MovieListItemAdapter(this,this.movieViewModelList,movieList));
+            }
+            else{
+                movieList.setLayoutManager((new GridLayoutManager(this, 2)));
+                movieList.setAdapter(new MovieGridItemAdapter(this, this.movieViewModelList,movieList));
+            }
         }
     }
 
+    /**
+     * Initialise le bouton de la page principal qui inverse les vues
+     */
     public void setButton(){
         ImageButton switch_button = findViewById(R.id.change_display);
         switch_button.setOnClickListener(new View.OnClickListener() {
@@ -76,6 +91,9 @@ public class MainList extends AppCompatActivity implements ViewContract {
         });
     }
 
+    /**
+     * Inverse l'attribut isListView
+     */
     public void switch_view(){
         isListView = !isListView;
         displayMovies();
