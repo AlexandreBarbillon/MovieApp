828f4873dd90291dd80d6b4d576b4697c06f03e6
Alex <alexandre.barbillon@gmail.com>
2019-12-19 14:48:38 +0100

    Dernière doc

--- app/src/main/java/barbillon/movieapp/api/config/ApiConfig.java
+++ app/src/main/java/barbillon/movieapp/api/config/ApiConfig.java
@@ -3,7 +3,7 @@ package barbillon.movieapp.api.config;
 import barbillon.movieapp.api.config.enums.Languages;
 
 /**
- * ApiConfig est un configurateur pour l'api, elle contient la langue de base de l'API, l'url de l'API et permet de récupérer la clé d'Api de l'objet ApiKey (qui n'est pas sur le dépot git)
+ * ApiConfig est un configurateur pour l'api, elle contient la langue de base de l'API, l'url de l'API et permet de r&eacute;cup&eacute;rer la cl&eacute; d'Api de l'objet ApiKey (qui n'est pas sur le d&eacute;pot git)
  */
 public class ApiConfig {
     public static Languages LANGUAGE = Languages.FRENCH;
